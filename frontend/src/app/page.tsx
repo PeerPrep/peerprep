6d43bae720d1596dd1637db71e52a6479434d454
elroygohjy <e0543508@u.nus.edu>
2023-09-12 22:16:27 +0800

    fix(style): update styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
         className="-z-10 opacity-50"
         layout="fill"
       />
-      <section className="flex flex-col items-center py-36 px-24 gap-4 z-20">
-        <h1 className="text-7xl text-white font-bold text-center m-5 max-w-10xl">
+      <section className="flex flex-col items-center py-24 lg:py-36 px-24 gap-4 z-20">
+        <h1 className="text-5xl text-white font-bold text-center m-5 lg:text-7xl">
           Streamlining Technical Interview Preparation.
         </h1>
-        <h2 className="text-xl text-center text-white font-medium max-w-3xl m-5">
+        <h2 className="text-xl text-center text-white font-medium">
           Collaborative mock interviews to boost your confidence and nail your
           dream job interviews.
         </h2>
         <Button
-          className="btn-accent w-36 m-6"
+          className="btn-accent w-36 m-2"
           children={<span>Get Started!</span>}
         />
       </section>
