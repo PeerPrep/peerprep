74f8dec06ac49fa649ca5727c3cda2d868f51d65
kevin <skullspine9@gmail.com>
2023-09-12 17:12:02 +0800

    feat: add link to navbar, make font smaller

--- frontend/src/app/components/navbar/Navbar.tsx
+++ frontend/src/app/components/navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import { IoPeopleCircleSharp } from "react-icons/io5";
 import Button from "../button/Button";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="flex justify-between bg-neutral p-2 px-12 items-center">
-        <div className="text-4xl text-white font-bold flex items-center">
-          <IoPeopleCircleSharp className="text-base-100 text-5xl" />
+        <div className="text-3xl text-white font-bold flex items-center">
+          <Link href="/">
+            <IoPeopleCircleSharp className="text-base-100 text-5xl cursor-pointer" />
+          </Link>
           PeerPrep
         </div>
         <Button
