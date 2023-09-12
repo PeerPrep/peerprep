a78f677e57226571242d8b45879a2a4da98d71d8
elroygohjy <e0543508@u.nus.edu>
2023-09-12 16:15:39 +0800

    fix(landing): update styling for h1 text

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
         fill
       />
       <section className="flex flex-col items-center py-36 px-24 gap-4 z-20">
-        <h1 className="text-7xl text-white font-bold">Let's Prep Together!</h1>
+        <h1 className="text-7xl text-white font-bold text-center">
+          Let's Prep Together!
+        </h1>
         <h2 className="text-xl text-center text-white font-medium max-w-xl">
           Collaborative Mock Interviews to Boost Your Confidence and Nail Your
           Dream Job Interviews
