eb72a4ef9c72cf7cc42a1aca3e3d77c7ebcc168e
elroygohjy <e0543508@u.nus.edu>
2023-09-12 14:06:37 +0800

    feat(init): init frontend

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+    daisyui: {
+      base: false,
+      themes: [
+        {
+          myTheme: {
+            primary: "#383D4B",
+            secondary: "#22242D",
+            accent: "#5ecce0",
+            neutral: "#404559",
+            "base-100": "#1A1C2C",
+            info: "#3abff8",
+            success: "#36d399",
+            warning: "#fbbd23",
+            error: "#f87272",
+          },
+        },
+      ],
+    },
+    plugins: [require("daisyui")],
+  },
+  plugins: [],
+};
+export default config;
