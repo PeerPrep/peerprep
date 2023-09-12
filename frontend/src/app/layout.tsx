31831ba8a5c56462a74cbd9d253c36f12911415a
Siva <sivayogasubramanian@gmail.com>
2023-09-12 20:49:05 +0800

    Init deployment

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,15 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Peerprep",
-  description: "Practise!",
+  title: 'Peerprep',
+  description: 'Practise!',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" data-theme="myTheme">
       <body className={montserrat.className}>{children}</body>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
@@ -14,19 +14,19 @@ const config: Config = {
     themes: [
       {
         myTheme: {
-          primary: "#383D4B",
-          secondary: "#22242D",
-          accent: "#5ecce0",
-          neutral: "#404559",
-          "base-100": "#1A1C2C",
-          info: "#3abff8",
-          success: "#36d399",
-          warning: "#fbbd23",
-          error: "#f87272",
+          primary: '#383D4B',
+          secondary: '#22242D',
+          accent: '#5ecce0',
+          neutral: '#404559',
+          'base-100': '#1A1C2C',
+          info: '#3abff8',
+          success: '#36d399',
+          warning: '#fbbd23',
+          error: '#f87272',
         },
       },
     ],
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
 };
 export default config;
