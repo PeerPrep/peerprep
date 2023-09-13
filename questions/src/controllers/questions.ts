dc16a2c8f9bce9f55ce1f7bc845b5f9cef1e8a62
Siva <sivayogasubramanian@gmail.com>
2023-09-13 19:08:06 +0800

    Complete CRUD

--- questions/src/controllers/questions.ts
+++ questions/src/controllers/questions.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { QuestionDao } from "../models/questions";
 import { ApiResponse, EMPTY_OBJECT, StatusMessageType } from "../types";
+import { handleCustomError, handleServerError } from "../utils";
 
 // GET /questions/:id
 export const getQuestion = async (
@@ -10,28 +11,18 @@ export const getQuestion = async (
   try {
     const id = req.params.id;
     if (!id) {
-      const response: ApiResponse = {
-        payload: EMPTY_OBJECT,
-        statusMessage: {
-          type: StatusMessageType.ERROR,
-          message: "No ID provided",
-        },
-      };
-
-      res.status(400).json(response);
+      handleCustomError(res, {
+        type: StatusMessageType.ERROR,
+        message: "Question ID must be provided",
+      });
     }
 
     const question = await QuestionDao.getQuestionById(id);
     if (!question) {
-      const response: ApiResponse = {
-        payload: EMPTY_OBJECT,
-        statusMessage: {
-          type: StatusMessageType.ERROR,
-          message: "No question found",
-        },
-      };
-
-      res.status(404).json(response);
+      handleCustomError(res, {
+        type: StatusMessageType.ERROR,
+        message: "Question not found",
+      });
     }
 
     const response: ApiResponse = {
@@ -40,16 +31,7 @@ export const getQuestion = async (
     };
     res.status(200).json(response);
   } catch (error) {
-    console.log(error);
-    const response: ApiResponse = {
-      payload: EMPTY_OBJECT,
-      statusMessage: {
-        type: StatusMessageType.ERROR,
-        message: "Something went wrong",
-      },
-    };
-
-    res.status(500).json(response);
+    handleServerError(error, res);
   }
 };
 
@@ -61,33 +43,93 @@ export const createQuestion = async (
   try {
     const { title, description } = req.body;
     if (!title || !description) {
-      const response: ApiResponse = {
-        payload: EMPTY_OBJECT,
-        statusMessage: {
-          type: StatusMessageType.ERROR,
-          message: "Title or description must provided",
-        },
-      };
-
-      res.status(400).json(response);
+      handleCustomError(res, {
+        type: StatusMessageType.ERROR,
+        message: "Title and description must be provided",
+      });
     }
 
     const question = await QuestionDao.createQuestion(title, description);
     const response: ApiResponse = {
       payload: question,
-      statusMessage: null,
+      statusMessage: {
+        type: StatusMessageType.SUCCESS,
+        message: "Question created successfully",
+      },
     };
     res.status(201).json(response);
   } catch (error) {
-    console.log(error);
-    const response: ApiResponse = {
-      payload: EMPTY_OBJECT,
-      statusMessage: {
+    handleServerError(error, res);
+  }
+};
+
+// PUT /questions/:id
+export const updateQuestion = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const id = req.params.id;
+    const { title, description } = req.body;
+    if (!id || !title || !description) {
+      handleCustomError(res, {
         type: StatusMessageType.ERROR,
-        message: "Something went wrong",
+        message: "Question ID, title, and description must be provided",
+      });
+    }
+
+    const question = await QuestionDao.updateQuestion(id, title, description);
+    if (!question) {
+      handleCustomError(res, {
+        type: StatusMessageType.ERROR,
+        message: "Question not found",
+      });
+    }
+
+    const response: ApiResponse = {
+      payload: question,
+      statusMessage: {
+        type: StatusMessageType.SUCCESS,
+        message: "Question updated successfully",
       },
     };
+    res.status(200).json(response);
+  } catch (error) {
+    handleServerError(error, res);
+  }
+};
 
-    res.status(500).json(response);
+// DELETE /questions/:id
+export const deleteQuestion = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      handleCustomError(res, {
+        type: StatusMessageType.ERROR,
+        message: "Question ID must be provided",
+      });
+    }
+
+    const question = await QuestionDao.deleteQuestion(id);
+    if (!question) {
+      handleCustomError(res, {
+        type: StatusMessageType.ERROR,
+        message: "Question not found",
+      });
+    }
+
+    const response: ApiResponse = {
+      payload: question,
+      statusMessage: {
+        type: StatusMessageType.SUCCESS,
+        message: "Question deleted successfully",
+      },
+    };
+    res.status(200).json(response);
+  } catch (error) {
+    handleServerError(error, res);
   }
 };
--- questions/src/models/questions.ts
+++ questions/src/models/questions.ts
@@ -9,12 +9,33 @@ const QuestionModel = mongoose.model("Question", QuestionSchema);
 
 const getQuestionById = async (id: string) =>
   QuestionModel.findById(id).then((question) => question?.toObject());
+
 const createQuestion = async (title: string, description: string) => {
   const question = new QuestionModel({ title, description });
   return question.save().then((question) => question.toObject());
 };
 
+const updateQuestion = async (
+  id: string,
+  title: string,
+  description: string
+) => {
+  const updatedQuestion = await QuestionModel.findByIdAndUpdate(
+    id,
+    { title, description },
+    { new: true }
+  );
+  return updatedQuestion?.toObject();
+};
+
+const deleteQuestion = async (id: string) => {
+  const deletedQuestion = await QuestionModel.findByIdAndDelete(id);
+  return deletedQuestion?.toObject();
+};
+
 export const QuestionDao = {
   getQuestionById,
   createQuestion,
+  updateQuestion,
+  deleteQuestion,
 };
