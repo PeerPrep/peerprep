f67b0af3f0ad1285e466cf1cbeba8fca8a27dcff
Elroy Goh Jun Ying <E0543508@u.nus.edu>
2023-09-13 18:01:33 +0800

    fix(style): sort tailwind class for landing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
         className="-z-10 opacity-50"
         layout="fill"
       />
-      <section className="flex flex-col items-center py-24 lg:py-36 px-24 gap-4 z-20">
-        <h1 className="text-5xl text-white font-bold text-center m-5 lg:text-7xl">
+      <section className="z-20 flex flex-col items-center gap-4 px-24 py-24 lg:py-36">
+        <h1 className="m-5 text-center text-5xl font-bold text-white lg:text-7xl">
           Streamlining Technical Interview Preparation.
         </h1>
-        <h2 className="text-xl text-center text-white font-medium">
+        <h2 className="text-center text-xl font-medium text-white">
           Collaborative mock interviews to boost your confidence and nail your
           dream job interviews.
         </h2>
         <Button
-          className="btn-accent w-36 m-2"
+          className="btn-accent m-2 w-36"
           children={<span>Get Started!</span>}
         />
       </section>
