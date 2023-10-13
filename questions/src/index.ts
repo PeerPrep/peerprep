a22b1e64df5d366ba1f274edd244d3586a587dc8
Siva <sivayogasubramanian@gmail.com>
2023-10-13 21:41:50 +0800

    Query users service for role

--- questions/src/index.ts
+++ questions/src/index.ts
@@ -33,12 +33,10 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(compression());
 app.use(bodyParser.json());
-app.use(getFirebaseMiddleware(firebaseAuth));
+app.use("/api/v1/", getFirebaseMiddleware(firebaseAuth), router());
 
 const server = http.createServer(app);
 
 server.listen(4000, () => {
   console.log("Server is listening on port 4000");
 });
-
-app.use("/api/v1/", router());
