ed26aa801ab94520f711187f71192253bf2414cf
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-11-01 21:43:26 +0800

    allow delete profile

---
+++ users/src/profile/delete.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { Profile } from "../entities/Profile";
+import { ApiResponse, StatusMessageType } from "../types";
+import { handleServerError } from "../utils";
+import { getAuth } from "firebase-admin/auth";
+import { Activity } from "../entities/Activity";
+
+export async function deleteProfileHandler(req: Request, res: Response) {
+  const em = req.orm.em.fork();
+  const uid = req.userToken.uid;
+
+  try {
+    await em.nativeDelete(Profile, { uid: uid });
+    await em.nativeDelete(Activity, { uid: uid });
+
+    const response: ApiResponse = {
+      statusMessage: {
+        message: "Profile deleted successfully",
+        type: StatusMessageType.SUCCESS,
+      },
+      payload: {},
+    };
+    res.status(200).send(response);
+  } catch (err: any) {
+    handleServerError(err, res);
+  }
+}
