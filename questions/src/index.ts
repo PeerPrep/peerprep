df8945a5c16bf27a90d2c80ebb3165115038ebac
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-11-11 00:28:08 +0800

    remove firebase and serverless questions service

--- questions/src/index.ts
+++ questions/src/index.ts
@@ -3,12 +3,8 @@ import compression from "compression";
 import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { applicationDefault, initializeApp } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
 import http from "http";
 import mongoose from "mongoose";
-import getFirebaseMiddleware from "./middleware/auth";
-import decryptRequestBody from "./middleware/serverless";
 import { normalRouter } from "./router";
 
 dotenv.config();
@@ -16,13 +12,6 @@ dotenv.config();
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL);
 mongoose.connection.on("error", console.error);
-
-const firebaseApp = initializeApp({
-  credential: applicationDefault(),
-  storageBucket: process.env.BUCKET_NAME,
-});
-
-const firebaseAuth = getAuth(firebaseApp);
 
 const corsOptions: CorsOptions = {
   origin: "*",
@@ -33,8 +22,7 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(compression());
 app.use(bodyParser.json());
-app.use("/api/v1/", getFirebaseMiddleware(firebaseAuth), normalRouter());
-app.use("/api/serverless/", decryptRequestBody(), normalRouter());
+app.use("/api/v1/", normalRouter());
 
 const server = http.createServer(app);
 
--- questions/src/router/index.ts
+++ questions/src/router/index.ts
@@ -1,14 +1,9 @@
 import express from "express";
-import { questions, serverlessQuestions } from "./questions";
+import { questions } from "./questions";
 
 const router = express.Router();
 
 export const normalRouter = (): express.Router => {
   questions(router);
-  return router;
-};
-
-export const serverlessRouter = (): express.Router => {
-  serverlessQuestions(router);
   return router;
 };
