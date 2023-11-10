a62d52bfcd9d2df89acb2e36a05e1fcc744b6869
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-11-10 23:30:37 +0800

    remove for a2

--- innkeeper/src/controllers/index.ts
+++
@@ -1,8 +0,0 @@
-import { InnkeeperIoSocket, InnkeeperOtherSockets, NotificationMessage } from '../types';
-import { SHOULD_LOG } from '../utils';
-
-export const sendNotification = (socket: InnkeeperIoSocket | InnkeeperOtherSockets, { type, message }: NotificationMessage) => {
-  const socketData = `${socket.id} (userId: ${socket.data.userId})`;
-  SHOULD_LOG && console.log(`[OTHER][NOTIF] Sending ${type} notification to ${socketData}: ${message}`);
-  socket.emit('sendNotification', { type, message });
-};
